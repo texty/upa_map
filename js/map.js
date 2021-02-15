e76605ae85aaabd2480c6f1b1508aeb86caa2ccc
ptrBodnar <peterbodnar1213@gmail.com>
2021-02-15 23:40:05 +0200

    added stupid filter

--- js/map.js
+++ js/map.js
@@ -46,6 +46,7 @@ Promise.all([
 
     const upa_places = new maptalks.VectorLayer('upa').addTo(map);
     var label_point = new maptalks.VectorLayer('label').addTo(map);
+    
 
     const linesLayer = new maptalks.VectorLayer('lines', {
         // 'opacity': 0.2
@@ -72,6 +73,9 @@ Promise.all([
                 'markerLineWidth': 0.8,
                 'markerWidth': 10,
                 'markerHeight': 10
+            },
+            properties : {
+                'date' : d.creation_date
             }
         }
 
@@ -96,7 +100,10 @@ Promise.all([
                             'markerLineWidth': 0.8,
                             'markerWidth': 10,
                             'markerHeight': 10
-                        }
+                        },
+                    properties : {
+                        'date' : d.creation_date
+                    }
                     }
                 );
               } catch (error) {
@@ -114,8 +121,8 @@ Promise.all([
         var line = new maptalks.ArcConnectorLine(src, dst, {
             arcDegree: 90,
             showOn: 'always',
-            arrowStyle: 'classic',
-            arrowPlacemet: 'vertex-last', //vertex-first, vertex-last, vertex-firstlast, point
+            // arrowStyle: 'classic',
+            // arrowPlacemet: 'vertex-last', //vertex-first, vertex-last, vertex-firstlast, point
             symbol: {
                 'fillColor': "#00000",
                 'fillOpacity': 1,
@@ -132,162 +139,35 @@ Promise.all([
 
         }
 
-        // data[1].forEach(function (d) {
-        //     d.child_lat = +d.child_lat;
-        //     d.child_lon = +d.child_lon;
-
-        //     new maptalks.Marker(
-        //         [d.child_lon, d.child_lat], {
-        //             'properties': {
-        //                 'name': ''
-        //             },
-        //             'symbol': {
-        //                 'textFaceName': 'sans-serif',
-        //                 'textName': d.place, //value from name in geometry's properties
-        //                 'textWeight': 'normal', //'bold', 'bolder'
-        //                 'textStyle': 'normal', //'italic', 'oblique'
-        //                 'textSize': 12,
-        //                 'textFont': null, //same as CanvasRenderingContext2D.font, override textName, textWeight and textStyle
-        //                 'textFill': '#34495e',
-        //                 'textOpacity': 1,
-        //                 'textHaloFill': '#E6EBE6',
-        //                 'textHaloRadius': 5,
-        //                 'textWrapWidth': null,
-        //                 'textWrapCharacter': '\n',
-        //                 'textLineSpacing': 0,
-
-        //                 'textDx': 0,
-        //                 'textDy': 0,
-
-        //                 'textHorizontalAlignment': 'middle', //left | middle | right | auto
-        //                 'textVerticalAlignment': 'middle', // top | middle | bottom | auto
-        //                 'textAlign': 'center' //left | right | center | auto
-        //             }
-        //         }
-        //     ).addTo(label_point);
-        // });
-
-
-
     });
 
-    // function drawLayer(df, target_layer) {
 
-    //     df.forEach(function (d) {
-    //         d.child_lat = +d.child_lat;
-    //         d.child_lon = +d.child_lon;
-    //         d.parent_lat = +d.parent_lat;
-    //         d.parent_lon = +d.parent_lon;
-    //     });
+    d3.select("#filter").on("click", function() {
+        console.log("aaas")
+        function compare_strings(st, st_2){ 
+            console.log(st, st_2)
+            console.log("yes")
+            
+            return True
+        }
 
+        function filter() {
+            upa_places._geoList
+              .forEach(function (feature) {
 
-    //     // var nested = d3.nest()
-    //     //     .key(function (d) { return d.parent_node })
-    //     //     .entries(df);
-
-
-
-
-    //     console.log(nested);
-
-    //     // var color = d3.scaleOrdinal()
-    //     //     .domain(["coal", "grain", "ore", "stone"])
-    //     //     .range(["#000000", "#15CF74", "#FF3A44", "#FFD600"]);
-
-
-    //     // максимальна к-ть вагонів для line width domain
-    //     // var maxWag = d3.max(df, function (d) { return +d.no_wagons });
-
-    //     // var scaleRadius = d3.scaleLinear()
-    //     //     .domain([0, maxWag])
-    //     //     .range([5, 25]);
-
-    //     // var scaleLineWidth = d3.scaleLinear()
-    //     //     .domain([0, maxWag])
-    //     //     .range([1, 4]);
-
-
-
-    //     nested.forEach(function (d) {
-
-    //         // let radius = scaleRadius(d.values[0].no_wagons);
-    //         // let lineWidth = scaleLineWidth(d.values[0].no_wagons);
-    //         // blue circle
-    //         var src = new maptalks.Marker(
-    //             [d.values[0].longitude, d.values[0].latitude], {
-    //             symbol: {
-    //                 'markerType': 'ellipse',
-    //                 'markerFill': "#FF3A44",
-    //                 'markerFillOpacity': 0.8,
-    //                 'markerLineColor': "white",
-    //                 'markerLineWidth': 0.8,
-    //                 'markerWidth': 20,
-    //                 'markerHeight': 20
-    //             }
-    //         }
-    //         );
-
-
-    //         addListen();
-
-    //         function addListen() {
-    //             //mousemove and touchmove is annoying, so not listening to it.
-    //             src.on('click', function () {
-    //                 linesLayer.forEach(function (l) {
-    //                     l.remove();
-    //                 });
-
-    //                 d.values.forEach(function (t) {
-    //                     var dst = new maptalks.Marker(
-    //                         [t.lon_receiver, t.lat_receiver], {
-    //                         'symbol': {
-    //                             'markerType': 'ellipse',
-    //                             'markerFill': '#E78B8A',
-    //                             'markerFillOpacity': 0.9,
-    //                             'markerLineColor': color(type),
-    //                             'markerLineWidth': 3,
-    //                             'markerWidth': 0,
-    //                             'markerHeight': 0
-    //                         }
-    //                     }
-    //                     );
-
-
-
-    //                     var line = new maptalks.ArcConnectorLine(src, dst, {
-    //                         arcDegree: 90,
-    //                         showOn: 'always',
-    //                         arrowStyle: 'classic',
-    //                         arrowPlacement: 'vertex-last', //vertex-first, vertex-last, vertex-firstlast, point
-    //                         symbol: {
-    //                             'fillColor': color(type),
-    //                             'fillOpacity': 1,
-    //                             'lineColor': color(type),
-    //                             'lineWidth': 1,
-    //                             //'lineWidth': lineWidth,
-    //                             'lineOpacity': 1
-    //                         }
-    //                     });
-
-    //                     linesLayer.addGeometry(line);
-    //                 });
-
-
-
-
-
-    //             });
-    //         }
-
-
-    //         target_layer.addGeometry(src);
-
-
-    //     });
-    // }
-
-
-    // drawLayer(data[1], upa_places);
-
+                if (feature.properties.date > "1942-01-01 00:00:00") {
+                feature._symbol.markerFill = "#7375d8";
+                feature.updateSymbol([
+                  {
+                    // 'markerFill': '#7375d8',
+                    // 'markerWidth': 100,
+                    // 'markerHeight': 100
+                  }
+                ]);
+                }
+              });
+          }
+          filter()
+    })
 
 });
